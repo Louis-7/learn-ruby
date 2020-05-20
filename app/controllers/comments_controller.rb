2ba223dd3a757b1118f72a039fc0150712f178ee
Louis-7 <louisgh.cn@gmail.com>
2020-05-20 17:58:30 +0800

    [update] figureout how controller interact with template

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
-    before_action :set_post
+    before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+    # GET /posts  
+    def index
+        @comments = Post.find(params[:post_id]).comments
+    end
+     
+    # GET /comments/1
+    def show
+    end
 
     def create
         @post.comments.create! comments_params
@@ -7,11 +16,8 @@ class CommentsController < ApplicationController
     end
 
     private
-        def set_post
-            @post = Post.find(params[:post_id])
-        end 
-
-        def comments_params
-            params.required(:comment).permit(:body)
-        end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Post.find(params[:post_id]).comments.find(params[:id])
+    end
 end
